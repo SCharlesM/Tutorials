95cef8f470af9d9df8aa5969e1390ce1a3e7162c
SCharles <97812129+SCharlesM@users.noreply.github.com>
2025-02-11 08:44:32 +0000

    working on dog.py and car.py

---
+++ oop_realpython/car.py
@@ -0,0 +1,21 @@
+"""
+A simple car class
+Exercise from Object Oriented progreamming tutorial from Real Python
+https://realpython.com/python3-object-oriented-programming/
+"""
+class Car:
+
+    def __init__(self, colour, mileage):
+        
+        self.colour = colour
+        self.mileage = mileage
+    
+    def __str__(self):
+        
+        return f'The {self.colour} car has {self.mileage:,} miles'  #the extra comma after mileage is a format specifier to group thousands with ,
+
+car1 = Car('blue', 20000)
+car2 = Car('red', 30000)
+
+print(car1)
+print(car2)
--- oop_realpython/dog.py
+++ oop_realpython/dog.py
@@ -2,5 +2,25 @@
 Object oriented programming tutorial from Real Python
 https://realpython.com/python3-object-oriented-programming/
 """
-class Dog()
-    pass
+class Dog:
+    species = "Canis Familiaris"            #class attribute
+
+    def __init__(self, name, age):
+        self.name = name                    #instance attributes
+        self.age = age
+    
+    #instance method
+    #def description(self):
+    def __str__ (self):
+        return f'{self.name} is {self.age} years old'
+
+    #another instance method
+    def speak(self, sound):
+        return f'{self.name} says {sound}'
+
+
+fido = Dog('Fido', 4)
+miles = Dog('Miles', 5)
+
+print(miles)
+print(miles.speak('woof, woof'))
