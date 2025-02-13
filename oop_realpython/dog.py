1c4ea9a75a93db4a663fb9dfcd8cbae92231c670
SCharles <97812129+SCharlesM@users.noreply.github.com>
2025-02-13 19:10:14 +0000

    updating dog.py

--- oop_realpython/dog.py
+++ oop_realpython/dog.py
@@ -5,9 +5,10 @@ https://realpython.com/python3-object-oriented-programming/
 class Dog:
     species = "Canis Familiaris"            #class attribute
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name                    #instance attributes
         self.age = age
+        self.breed = breed
     
     #instance method
     #def description(self):
@@ -18,9 +19,16 @@ class Dog:
     def speak(self, sound):
         return f'{self.name} says {sound}'
 
-
+"""
+initialising an object with name and age attributes
 fido = Dog('Fido', 4)
 miles = Dog('Miles', 5)
 
 print(miles)
 print(miles.speak('woof, woof'))
+"""
+miles = Dog('Miles', 4, 'Jack Russell Terrier')
+buddy = Dog('Buddy', 9, 'Dachshund')
+jack = Dog('Jack', 3, 'Bulldog')
+jim = Dog('Jim', 5, 'Bulldog')
+
